5798fb6408c2e8365578133d5f4a0cb110ef0ce8
I don’t codddde <120886940+asc2563@users.noreply.github.com>
2026-08-06 10:16:06 -0700

    stuff

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,1 @@
-// place files you want to import through the `$lib` alias in this folder.
+//Note to self: place files you want to import through the `$lib` alias in this folder and i can use this file for js $lib
